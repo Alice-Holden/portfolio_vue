f36f2d5d7ac87065ec03205d93726708512a2245
Наталья <natalia@MacBook-Pro-Natala.local>
2023-04-07 10:34:12 +0700

    First commit

---
+++ src/components/HomePage.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="home-page">
+    <div class="home-page__word-blocks">
+
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HomePage.vue"
+}
+</script>
+
+<style scoped lang="scss">
+.home-page{
+  background: #ECC34E;
+  width: 100vw;
+  height: 100vh;
+
+}
+</style>
